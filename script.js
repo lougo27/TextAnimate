d60a9da7e778e2f163026b9f9f23297f9150f2a0
lougo27 <louanegouin@gmail.com>
2025-05-12 22:45:44 +0200

    test5_github

--- script.js
+++ script.js
@@ -448,7 +448,7 @@ setTimeout(showWords, 500);
 //LETRES QUI BOUGENT I M GOING TO DO IT
 const elements = document.querySelectorAll('.bouge');
 
-const blockPosition = 1500;
+const blockPosition = 1000;
 const finalPosition = 2500;
 
 let lastWheelTime = performance.now();
